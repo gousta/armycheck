b8df9c94e2707b6674c5b4a2188f39968260de5c
Stratos Giouldasis <giouldasis.stratos@gmail.com>
2013-12-10 16:42:57 +0200

    Separated configuration values. Removed personal information.

--- check.php
+++ check.php
@@ -1,16 +1,19 @@
-<?php require('simple_html_dom.php');
+<?php
+
+require('simple_html_dom.php');
+require('config.php');
 
 $cache = dirname(__FILE__).'/katataxi.txt';
 
 $form = 'http://www.stratologia.gr/proepiskophsh_stoixoivn';
 $data = array(
 	'mode' => 'katataxi',
-	'asg' => '154',
-	'asm' => '002801',
-	'klash' => '2011',
-	'lastname' => 'ΓΙΟΥΛΔΑΣΗΣ',
-	'firstname' => 'ΕΥΣΤΡΑΤΙΟΣ',
-	'yearofbirth' => '1990',
+	'asg' => $user['asg'],
+	'asm' => $user['asm'],
+	'klash' => $user['klash'],
+	'lastname' => $user['lastname'],
+	'firstname' => $user['firstname'],
+	'yearofbirth' => $user['yearofbirth'],
 	'pskForm' => true,
 );
 
@@ -25,32 +28,7 @@ $response['info'] = curl_getinfo($ch);
 
 $html = str_get_html($response['data']);
 
-/*
-$asg = $html->find('input[name=asg]', 0);
-$asm = $html->find('input[name=asm]', 0);
-$klash = $html->find('input[name=klash]', 0);
-$lastname = $html->find('input[name=lastname]', 0);
-$firstname = $html->find('input[name=firstname]', 0);
-$yearofbirth = $html->find('input[name=yearofbirth]', 0);
-
-$nomarxia = $html->find('input[name=nomarxiaMaLektiko]', 0);
-$ota = $html->find('input[name=otaMaLektiko]', 0);
-$diamerisma = $html->find('input[name=diamMaLektiko]', 0);
-*/
-
 $katataxi = $html->find('div#enhmKatataxisDiv', 0);
-
-/*
-echo 'Στρατιωτικός Αριθμός (ΣΑ): '.$asg->value.'/'.$asm->value.'/'.$klash->value;
-echo '<br/>';
-echo 'Νομαρχία: '.$nomarxia->value;
-echo '<br/>';
-echo 'Δήμος: '.$ota->value;
-echo '<br/>';
-echo 'Τοπικό Διαμέρισμα: '.$diamerisma->value;
-echo '<hr/>';
-echo $katataxi->plaintext;
-*/
 
 $katataxiText = trim(preg_replace('/\s+/', ' ', $katataxi->plaintext));
 
@@ -58,12 +36,10 @@ $fileData = file_get_contents($cache);
 
 if($fileData !== $katataxiText) {
 	
-	$to      = 'giouldasis.stratos@gmail.com';
+	$to      = $mail['to'];
 	$subject = 'ARMY STATUS';
 	$message = $katataxiText;
-	$headers = 'From: mail@giouldasis-stratos.com' . "\r\n" .
-	'Reply-To: mail@giouldasis-stratos.com' . "\r\n" .
-	'X-Mailer: PHP/' . phpversion();
+	$headers = 'From: ' . $mail['from'] . "\r\n" . 'Reply-To: ' . $mail['from'] . "\r\n" . 'X-Mailer: PHP/' . phpversion();
 	
 	mail($to, $subject, $message, $headers);
 
---
+++ config.php
@@ -0,0 +1,17 @@
+<?php
+
+//Stoixeia Stratiwti
+//[asg]/[asm]/[klash] eg. 154/009999/2011
+
+$user = array(
+	'asg' => '', // 3 prwta noumera tou Stratiwtikou arithmou
+	'asm' => '', // epomena 6 noumera tou Stratiwtikou arithmou
+	'klash' => '', // teleutaia 4 noumera tou Stratiwtikou arithmou (etos)
+	'lastname' => '', // epitheto, kefalaia, ellinika
+	'firstname' => '', // onoma, kefalaia, ellinika
+	'yearofbirth' => '', // mono to etos gennisis. eg. 1990
+);
+
+//Stoixeia paralipti email
+$mail['to'] = ''; //receiver@example.com
+$mail['from'] = ''; //sender@example.com
